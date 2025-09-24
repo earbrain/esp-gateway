7e408382ef92990fd1e8664076e63de793248b86
Junichi Sato <sat0b.3ee@gmail.com>
2025-09-24 14:22:22 +0900

    Simplify

--- cmake/esp_gateway_component.cmake
+++
@@ -1,8 +0,0 @@
-get_filename_component(_ESP_GATEWAY_ROOT "${CMAKE_CURRENT_LIST_DIR}/.." REALPATH)
-
-idf_component_register(
-    SRCS
-        "${_ESP_GATEWAY_ROOT}/src/gateway.cpp"
-    INCLUDE_DIRS
-        "${_ESP_GATEWAY_ROOT}/include"
-)
