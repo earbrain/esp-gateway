5441d6bd41f21164116c5848024713350f892428
Junichi Sato <sat0b.3ee@gmail.com>
2025-10-06 18:41:15 +0900

    Remove unused DeviceInfo serialization code

--- include/earbrain/gateway/device_info.hpp
+++ include/earbrain/gateway/device_info.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <optional>
 #include <string>
-#include <string_view>
 
 namespace earbrain {
 
@@ -11,10 +9,6 @@ struct DeviceInfo {
   std::string firmware_version;
   std::string build_time;
   std::string idf_version;
-
-  std::string to_json() const;
-
-  static std::optional<DeviceInfo> from_json(std::string_view json);
 };
 
 } // namespace earbrain
